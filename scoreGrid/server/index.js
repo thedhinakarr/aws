ab2664d5865e37f8b3752cfc4ca1104fe4d69c86
thedhinakarr <codedhinakarr@gmail.com>
2024-05-17 14:59:58 +0530

    Initial ScoreGrid

---
+++ scoreGrid/server/index.js
@@ -0,0 +1,49 @@
+import { readFile, writeFile } from 'fs/promises';
+import fs from 'fs';
+import http from 'http';
+import https from 'https';
+import url from 'url';
+
+const httpPort = 80;
+const httpsPort = 443;
+
+const httpServer = http.createServer((req, res) => {
+  const httpsUrl = `https://${req.headers.host}${req.url}`;
+  res.writeHead(301, { Location: httpsUrl });
+  res.end();
+});
+
+const httpsServer = https.createServer(
+  {
+    key: fs.readFileSync('certs/privkey.pem'),
+    cert: fs.readFileSync('certs/fullchain.pem')
+  }, async (req, res) => {
+
+
+    const parsedUrl = url.parse(req.url, true);
+
+    if (req.method == 'GET' && parsedUrl.path == '/logs') {
+      try {
+        res.statusCode = 200;
+        res.setHeader('content-type', 'text')
+        res.end("HOI");
+      } catch (error) {
+        console.log(error);
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      }
+    } else {
+      res.statusCode = 404;
+      res.end('Not found');
+    }
+  });
+
+
+
+httpServer.listen(httpPort, () => {
+  console.log(`Server is running at ${httpPort}`);
+});
+
+httpsServer.listen(httpsPort, () => {
+  console.log(`Server is running at ${httpsPort}`);
+})
